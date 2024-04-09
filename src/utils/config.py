eb37d83d94fda1f7d3bbfb40c9d76e077de28323
algnot <thanawat.tongla@lmwn.com>
2024-04-09 12:38:35 +0700

    add get from unleash

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,17 +1,36 @@
 from dotenv import load_dotenv
+from requests import get
 import os
 
 
 class Config:
 
     def __init__(self):
+        self.unleash_endpoint = os.environ["UNLEASH_ENDPOINT"]
+        self.unleash_api_key = os.environ["UNLEASH_API_KEY"]
         load_dotenv()
 
-    def get(self, name: str, defualtVakue=""):
+    def get(self, name: str, default_value=""):
         try:
-            if name in os.environ:
-                return os.environ[name]
+            url = f"{self.unleash_endpoint}/api/admin/projects/default/features/{name}/environments/development/variants"
+            api_key = self.unleash_api_key
 
-            return defualtVakue
+            res = get(url=url, headers={
+                "Authorization": api_key,
+                "Content-Type": "application/json"
+            })
+
+            if res.status_code != 200:
+                if name in os.environ:
+                    return os.environ[name]
+                return default_value
+
+            return res.json().get("variants", [{}])[0].get("payload", {}).get("value", "")
+
         except Exception:
-            return defualtVakue
+            try:
+                if name in os.environ:
+                    return os.environ[name]
+                return default_value
+            except Exception:
+                return default_value
